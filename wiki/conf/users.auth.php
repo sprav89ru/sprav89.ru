f89904ce0cc458ccd0646bef04b74c9603282f5a
sprav89ru <sprav89@mail.ru>
2015-08-17 09:32:04 +0600

    Загрузка wiki Общая синхронизация

---
+++ wiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$LClDFjcc$BGlA6mfdZAce1ylsHGFH80:Андрей Овчинников:sprav89@mail.ru:admin,user
